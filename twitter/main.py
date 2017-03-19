5a3233ae92e7fd8feb8557ef1164bc45818fd032
eduardo-salazar <esalazar922@gmail.com>
2017-03-19 21:24:18 +0800

    Working connection to twitter

--- twitter/main.py
+++ twitter/main.py
@@ -1,12 +1,12 @@
 import tweepy
+import config
 from tweepy import OAuthHandler
 
-consumer_key = 'YOUR-CONSUMER-KEY'
-consumer_secret = 'YOUR-CONSUMER-SECRET'
-access_token = 'YOUR-ACCESS-TOKEN'
-access_secret = 'YOUR-ACCESS-SECRET'
-
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_secret)
+auth = OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_secret)
 
 api = tweepy.API(auth)
+
+for status in tweepy.Cursor(api.home_timeline).items(10):
+    # Process a single status
+    print(status.text)
